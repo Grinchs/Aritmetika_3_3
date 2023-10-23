31b60e4c1ed402c61e643542cba6243e7fcc2a0c
Grinchs <marcisg2345@gmail.com>
2023-10-23 23:15:26 +0300

    Add project files.

---
+++ Aritmetika_3_3/Program.cs
@@ -0,0 +1,34 @@
+﻿class Program
+{
+
+    static void Main()
+    {
+        Console.BackgroundColor = ConsoleColor.Green;
+        Console.ForegroundColor = ConsoleColor.Blue;
+        
+        Console.WriteLine("***                  ***    *****************            ");
+        Console.WriteLine("******            ******    *****************            ");
+        Console.WriteLine("***   ***      ***   ***    ***                          ");
+
+        Console.BackgroundColor= ConsoleColor.Red;
+        Console.ForegroundColor= ConsoleColor.Green;
+
+        Console.WriteLine("***      ******      ***    ***                          ");
+        Console.WriteLine("***                  ***    ***        ******            ");
+        Console.WriteLine("***                  ***    ***        ******            ");
+
+        Console.BackgroundColor = ConsoleColor.Blue;
+        Console.ForegroundColor = ConsoleColor.Yellow;
+
+        Console.WriteLine("***                  ***    ***           ***            ");
+        Console.WriteLine("***                  ***    *****************            ");
+        Console.WriteLine("***                  ***    *****************            ");
+
+        Console.BackgroundColor = ConsoleColor.Black;
+        Console.ForegroundColor = ConsoleColor.Magenta;
+    }
+}
+
+
+
+
